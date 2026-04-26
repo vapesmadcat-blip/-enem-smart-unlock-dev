06623627d16f46c8340f99d6cd7a790d29527294
vapesmadcat-blip <vapesmadcat@gmail.com>
2026-04-26 20:00:06 -0300

    Delete app/src/main/java/com/seuapp directory

--- app/src/main/java/com/seuapp/api/EnemApi.kt
+++
@@ -1,5 +0,0 @@
-interface EnemApi {
-
-    @GET("exams/2023/questions")
-    suspend fun getQuestions(): List<Question>
-}
--- app/src/main/java/com/seuapp/data/Question.kt
+++
@@ -1,9 +0,0 @@
-data class Question(
-    val title: String,
-    val alternatives: List<Alternative>
-)
-
-data class Alternative(
-    val text: String,
-    val isCorrect: Boolean
-)
