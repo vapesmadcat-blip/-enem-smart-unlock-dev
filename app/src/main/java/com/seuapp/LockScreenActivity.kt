7a26552d0df7232b84f08e645ae602060f53fcd9
vapesmadcat-blip <vapesmadcat@gmail.com>
2026-04-26 17:06:57 -0300

    Add LockScreenActivity for challenge loading screen

---
+++ app/src/main/java/com/seuapp/LockScreenActivity.kt
@@ -0,0 +1,15 @@
+class LockScreenActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        }
+
+        setContent {
+            Text("Carregando desafio ENEM...")
+        }
+    }
+}
